c8092c20a8b1ea1dd049978eb924558362670817
gunasourav33 <gunasourav33@gmail.com>
2026-04-01 21:38:14 -0500

    add vpc module outputs for subnet IDs, NAT IP, and route tables

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,44 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR block"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "nat_gateway_ip" {
+  description = "Elastic IP of the NAT Gateway"
+  value       = aws_eip.nat[0].public_ip
+}
+
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.main.id
+}
+
+output "public_route_table_id" {
+  description = "Public route table ID"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "Private route table ID"
+  value       = aws_route_table.private.id
+}
+
+output "availability_zones" {
+  description = "List of AZs used"
+  value       = data.aws_availability_zones.available.names
+}
